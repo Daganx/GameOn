91c3cf5bc7cdafaa2dd1925fcebfd2ae8078455b
Dagan <114420715+Daganx@users.noreply.github.com>
2023-12-05 14:50:13 +0100

    html_modification

---
+++ assets/js/modal.js
@@ -0,0 +1,28 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+const menuBurger = document.querySelector(".icon");
+
+menuBurger => menuBurger.addEventListener("click", editNav);
+
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+
