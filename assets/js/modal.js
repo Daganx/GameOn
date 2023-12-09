5486fee3b2408c3412b1d681764a881131455118
Dagan <114420715+Daganx@users.noreply.github.com>
2023-12-09 19:56:14 +0100

    html_css_ok

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -12,10 +12,12 @@ const menuBurger = document.querySelector(".icon");
 menuBurger => menuBurger.addEventListener("click", editNav);
 
 
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -25,4 +27,9 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
+modalClose.addEventListener("click", closeModal);
 
+function closeModal(){
+  modalbg.style.display = "none";
+}
