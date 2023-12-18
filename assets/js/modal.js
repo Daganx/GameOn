ab95cb00bf23dc10b41d6728af5d24d1a743807f
Dagan <114420715+Daganx@users.noreply.github.com>
2023-12-18 02:38:48 +0100

    js_v3

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,104 +1,172 @@
-// Burger Toggle
+// Burger Toggle :
 const menuBurger = document.querySelector(".icon");
-
 menuBurger.addEventListener('click', burgerToggle);
-
 function burgerToggle() {
   const topNav = document.getElementById("myTopnav");
   topNav.classList.toggle("responsive");
 }
-
-// DOM Elements
+// Modal Form :
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelector(".close");
 
-// launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-// close modal form
 modalClose.addEventListener("click", closeModal);
-
 function closeModal(){
   modalbg.style.display = "none";
 }
-
-// Variable INPUT Form
-
+// Inputs Form:
 const form = document.querySelector('form');
 const firstNameInput = document.getElementById("first");
 const lastNameInput = document.getElementById("last");
 const emailInput = document.getElementById("email");
 const birthdateInput = document.getElementById("birthdate");
-const quantityInput = document.getElementById("quantity");
-const locationInput = document.getElementsByName("location");
+const participationInput = document.getElementById("quantity");
+const radioButtons = document.querySelectorAll('input[name="location"]');
 const checkboxInput = document.getElementById("checkbox1");
-const checkbox2Input = document.getElementById("checkbox2");
 const btnSubmit = document.getElementsByClassName("btn-submit");
-
-// FUNCTION VERIF. FIRSTNAME
-
-function verifFirst(input){
+// Regex :
+const regExpText = new RegExp("^[A-Za-zéèê\-]+$");
+const regExpEmail = new RegExp('^[a-z0-9.-_]+[@]{1}[a-z0-9.-_]+[.]{1}[a-z]{2,4}$');
+// Error Msg : 
+const errorMessage = document.getElementById('firstNameError');
+// Functions Validations :
+function checkFirst(input) {
   const regExpText = new RegExp("^[A-Za-zéèê\-]+$");
-  if(regExpText.test(input.value)){
+  const errorMessage = document.getElementById('firstNameError');
+  if (input.value.length >= 2 && regExpText.test(input.value)) {
     console.log("PRENOM OK");
+    if (errorMessage) {
+      errorMessage.remove();
+    }
+    return true;
   } else {
-    console.log("PRENOM ERROR")
+    console.log("PRENOM ERROR");
+    if (!errorMessage) {
+      const errorElement = document.createElement('div');
+      errorElement.id = 'firstNameError';
+      errorElement.classList.add('error-message');
+      errorElement.textContent = 'Le prénom doit avoir au moins 2 caractères et ne doit contenir que des lettres.';
+      input.parentNode.appendChild(errorElement);
+    }
+    return false;
   }
-}
-
-// FUNCTION VERIF. LASTNAME
-
-function verifLast(input){
-  const regExpText = new RegExp("^[A-Za-zéèê\-]+$");
-  if(regExpText.test(input.value)){
+};
+function checkLast(input){
+  if(input.value.length >= 2 && regExpText.test(input.value)){
     console.log("NOM OK");
+    return true;
   } else {
-    console.log("NOM ERROR")
+    console.log("NOM ERROR");
+    return false;
   }
-}
-
-// FUNCTION VERIF EMAIL
-
-function verifEmail(input){
-  const regExpEmail = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$')
+};
+function checkEmail(input){
   if(regExpEmail.test(input.value)){
     console.log("EMAIL OK");
+    return true;
   } else {
     console.log("EMAIL ERROR");
+    return false;
   }
-}
+};
+function checkAge(){
+  const birthdate = new Date(birthdateInput.value);
+  const today = new Date();
+  const age = today.getFullYear() - birthdate.getFullYear();
+  if ( age >= 12){
+    console.log("AGE OK");
+    return true;
+  } else {
+    console.log("AGE MOINS 12");
+    return false;
+  }
+};
+function checkParticipation(input){
+  const value = parseInt(input.value, 10);
+  if(!isNaN(value) && value >= 0 && value <= 10){
+    console.log("PARTICIPATION OK");
+    return true;
+  } else {
+    console.log("PARTICIPATION ERROR")
+    return false;
+  }
+};
+function checkLocation() {
+  let isSelected = false;
 
-// EVENT LISTENER FORM SUBMIT 
-
+  radioButtons.forEach(button => {
+    if (button.checked) {
+      isSelected = true;
+      console.log('LOCATION OK');
+    }
+  });
+  return isSelected;
+};
+function checkConditions() {
+  if (checkboxInput.checked) {
+    console.log("CONDITIONS OK");
+    return true;
+  } else {
+    console.log("Veuillez accepter les conditions d'utilisation pour vous inscrire.");
+    return false;
+  }
+};
+// Submission Form :
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  verifFirst(firstNameInput);
-  verifLast(lastNameInput);
-  verifEmail(emailInput);
+  const isFirstNameValid = checkFirst(firstNameInput);
+  const isLastNameValid = checkLast(lastNameInput);
+  const isEmailValid = checkEmail(emailInput);
+  const isAgeValid = checkAge(birthdateInput);
+  const isParticipationValid = checkParticipation(participationInput);
+  const isLocationValid = checkLocation();
+  const isConditionValid = checkConditions();
+
+  if (isFirstNameValid &&
+      isLastNameValid &&
+      isEmailValid && 
+      isAgeValid &&
+      isParticipationValid &&
+      isLocationValid &&
+      isConditionValid) {
+    successModal.style.display = "block";
+  } else {
+    return false;
+  }
 });
-
-// EVENT LISTENER CHANGE SUR INPUT FIRSTNAME
-
+// Events Listeners :
 firstNameInput.addEventListener("change", () => {
-  verifFirst(firstNameInput);
+  checkFirst(firstNameInput);
 });
-
-// EVENT LISTENER CHANGE SUR INPUT LASTNAME
-
 lastNameInput.addEventListener("change", () =>{
-  verifLast(lastNameInput);
+  checkLast(lastNameInput);
 });
-
-// EVENT LISTENER CHANGE SUR INPUT EMAIL
-
 emailInput.addEventListener("change", () =>{
-  verifEmail(emailInput);
+  checkEmail(emailInput);
 });
+birthdateInput.addEventListener("change", () =>{
+  checkAge(birthdateInput);
+});
+participationInput.addEventListener("change", () =>{
+  checkParticipation(participationInput);
+});
+radioButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    console.log(`Bouton : ${button.value}`);
+  });
+});
+checkboxInput.addEventListener("change", () => {
+  checkConditions();
+});
+// Success Modal :
+const successModal = document.getElementById('modal-success');
+const successClose = document.getElementById('success-close');
+successClose.addEventListener('click', () => {
+  successModal.style.display = 'none';
+});
+
